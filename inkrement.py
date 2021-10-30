cdb7b6817af31acd88432d7a2b5c94a7d0136e53
Mamajonova <mamajonovagulkhayokhon@gmail.com>
2021-10-30 21:05:38 +0500

    Inkrement amaliga

---
+++ inkrement.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+"""
+a=4
+a+=4
+a+=4
+a+=4
+a+=4
+a+=4
+print(a)#Javob 24
+b="O'zbekiston"
+b+=" Respublikasi"
+b+=" 1-prezidenti"
+b+=" Islom"
+b+=" Abdug'anivich"
+b+=" Karimovning"
+b+=" Yuksak"
+b+=" ma'naviyat"
+b+=" yengilmas"
+b+=" kuch"
+b+=" asari"
+b+=" 2008-yilda"
+b+=" Ma'naviyat"
+b+=" nashriyoti"
+b+=" tomonidan"
+b+=" 176 betli"
+b+=" qattiq"
+b+=" muqovada"
+print(b)
+
+
