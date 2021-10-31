11e39202674b9ccd631a138d6f5e381117d16b1c
Mamajonova <mamajonovagulkhayokhon@gmail.com>
2021-10-31 22:51:47 +0500

    list bilan ishlash

--- sodda_hisoblash.py
+++ sodda_hisoblash.py
@@ -5,22 +5,76 @@ Spyder Editor
 This is a temporary script file.
 
 """
-a=int(input("1-sonni kiriting: "))
-b=int(input("2-sonni kiriting: "))
-print("yig'indisi=",a+b)
-print("ayirmasi=",a-b)
-print("ko'paytmasi=",a*b)
-print("bo'linmasi=",a/b)
-print("1-son 2-sidan kottami? ",a>b)
-print("1-son 2-sondan kichikmi? ",a<b)
-print("1-son ikkinchisiga tengmi? ",a==b)
+def salom_ber():
+    """Salom beruvchi funksiya"""
+    print("Assalomu alaykum")
+salom_ber()
 
-a="yer"
-b=" va"
-c=" osmon"
-print(a+b+c)
 
-ism=input("Ismingizni kiriting: ")
-yosh=int(input("Yoshingizni kiriting: "))
-print(f"{ism} , siz {yosh} yoshdasiz")
+def salom_ber(ism):
+    """Foydalanuvchi ismini qabul qilib salom beruvchi funksiya"""
+    print(f"Assalomu alaykum, hurmatli {ism.title()}")
+    
+salom_ber("Guli")
+salom_ber("ustoz")
+
+def toliq_ism(ism,familiya):
+    print(f"Foydalanuvchi ismi:{ism.title()}\n",
+          f"Foydalanuvchi familiyasi:{familiya.title()}")
+toliq_ism("Gulhayo", "Mamajonova")
+
+
+def yosh_hisobla(ism,t_yil):
+    print(f"{ism.title()},{2021-t_yil} yoshdasiz")
+yosh_hisobla("Gulhayo",2015)
+    
+yosh_hisobla(t_yil=2016,ism="guli")
+
+def yosh_hisobla(tugilgan_yil,joriy_yil=2021):
+    print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz")
+yosh_hisobla(1900,2020)
+yosh_hisobla(1900)
+
+def toliq_ism_yasa(ism,familiya):
+    toliq_ism=f"{ism} {familiya}"
+    return toliq_ism
+talaba=toliq_ism_yasa("Olim", "Hakimov")
+print(talaba)
+
+def toliq_ism_yasa(ism,familiya, otasining_ismi=" "):
+    if otasining_ismi:
+        toliq_ism=f"{ism} {familiya} {otasining_ismi}"
+    else:
+        toliq_ism=f"{ism} {familiya}"
+    return toliq_ism.title()
+print(toliq_ism_yasa("Olim", "Hakimov","Nizom ugli"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ uquvchilar_haqida_malumot_listda.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 31 21:56:30 2021
+
+@author: Premium
+"""
+
+list0=["id:","ism:","familiya:","kurs:","yosh:","yo'nalish:",
+       "fakultet:","viloyat:","qaysi tillarni biladi","o'rtacha o'qish bahosi"]
+list1=[1,"Gulhayo","Mamajonova",3,21,"Amaliy matematika va informatika",
+       "Amaliy matematika va intelektual texnalogiyalar","Farg'ona","o'zbek,rus",4.5]
+list2=[2,"Hilola","Bahriddinova",3,20,"Amaliy matematika va informatika",
+       "Amaliy matematika va intelektual texnologiyalar","Buxoro","o'zbek,rus,ingliz",4.8]
+list3=[3,"Ra'no","Abdumajidova",3,21,"Amaliy matematika va informatika",
+       "Amaliy matematika va intelektual texnologiyalar","Jizzax","o'zbek,rus",4.4]
+list4=[4,"Uljamal","Matimova",1,17,"Amaliy matematika",
+       "Amaliy matematika va intelektual texnologiyalar","Nukus","o'zbek,rus,qoraqalpoq",4.1]
+list5=[5,"Mohlaroy","Ergasheva",1,17,"Amaliy matematika",
+       "Amaliy matematika va intelektual texnologiyalar","Namangan","o'zbek,ingliz",4.2]
+list6=[6,"Muattar","Murodova",4,24,"Informatika",
+       "Amaliy matematika va intellektual texnologiyalar","Samarqand","o'zbek",4]
+list7=[7,"Shahlo","Olimova",1,18,"Matematika","Matematika","Andijon","o'zbek,tojik",3.8]
+list8=[8,"O'g'iloy","O'rolova",3,20,"Biofizika","Fizika","Farg'ona","o'zbek",4.1]
+list9=[9,"Zilola","Olimova",3,21,"Juralistika","Ijtimoiy ish","Farg'ona","o'zbek,ingliz",4.7]
+list10=[10,"Kim","Chi",2,22,"Kores tili","Xorijiy filologiya","Chang","kores,rus,ingliz,o'zbek,xitoy",5]
+print(list0)
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+print(list5)
+print(list6)
+print(list7)
+print(list8)
+print(list9)
+print(list10)
+
+print(list0[1])
+print(list1[1])
+print(list2[1])
+print(list3[1])
+print(list4[1])
+print(list5[1])
+print(list6[1])
+print(list7[1])
+print(list8[1])
+print(list9[1])
+print(list10[1])
+
+print(list0[2])
+print(list1[2])
+print(list2[2])
+print(list3[2])
+print(list4[2])
+print(list5[2])
+print(list6[2])
+print(list7[2])
+print(list8[2])
+print(list9[2])
+print(list10[2])
+
+print(list0[3])
+print(list1[3])
+print(list2[3])
+print(list3[3])
+print(list4[3])
+print(list5[3])
+print(list6[3])
+print(list7[3])
+print(list8[3])
+print(list9[3])
+print(list10[3])
+
+print(list0[4])
+print(list1[4])
+print(list2[4])
+print(list3[4])
+print(list4[4])
+print(list5[4])
+print(list6[4])
+print(list7[4])
+print(list8[4])
+print(list9[4])
+print(list10[4])
+
+print(list0[5])
+print(list1[5])
+print(list2[5])
+print(list3[5])
+print(list4[5])
+print(list5[5])
+print(list6[5])
+print(list7[5])
+print(list8[5])
+print(list9[5])
+print(list10[5])
+
+print(list0[6])
+print(list1[6])
+print(list2[6])
+print(list3[6])
+print(list4[6])
+print(list5[6])
+print(list6[6])
+print(list7[6])
+print(list8[6])
+print(list9[6])
+print(list10[6])
+
+print(list0[7])
+print(list1[7])
+print(list2[7])
+print(list3[7])
+print(list4[7])
+print(list5[7])
+print(list6[7])
+print(list7[7])
+print(list8[7])
+print(list9[7])
+print(list10[7])
+
+print(list0[8])
+print(list1[8])
+print(list2[8])
+print(list3[8])
+print(list4[8])
+print(list5[8])
+print(list6[8])
+print(list7[8])
+print(list8[8])
+print(list9[8])
+print(list10[8])
+
+print(list0[9])
+print(list1[9])
+print(list2[9])
+print(list3[9])
+print(list4[9])
+print(list5[9])
+print(list6[9])
+print(list7[9])
+print(list8[9])
+print(list9[9])
+print(list10[9])
+
+
+print("o'quvchilarning o'rtacha bahosi=",+(list1[9]+list2[9]+
+      list3[9]+list4[9]+list5[9]+list6[9]+list7[9]+list8[9]+list9[9]+list10[9])/10)
+print("o'quvchilarning o'rtacha yoshi=",+(list1[4]+list2[4]+
+      list3[4]+list4[4]+list5[4]+list6[4]+list7[4]+list8[4]+list9[4]+list10[4])/10)
+
+
+
+
+
+
+
+
+
+
+
+
