dcae3f208e910f57d7bc9ea7b202f14d1a772c01
Mamajonova <mamajonovagulkhayokhon@gmail.com>
2021-11-04 17:00:18 +0500

    file

---
+++ file bilan ishlash.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 14:38:26 2021
+
+@author: Premium
+"""
+#t="lili"
+#print(t)
+#print(len(t))
+#f="bor yoq"
+#print(len(f ))
+#file=open("olma.txt","w")
+#oqi=file.write("bildim")
+#print(oqi)
+#file=open("olma.txt","r")
+#oqi=file.read()
+#print(oqi)
+#file.close()
+#file=open("olma.txt","r")
+#oqi=file.read()
+#print(oqi)
+#ff=open("D:\GitHupRepos\GitHupRepos\olma.txt","r")
+#print(ff.read())
+#ff=open("olma.txt","r")
+#print(ff.read(6))
+
+
+#file=open("olma.txt","w")
+#oqi=file.write("tushundim")
+#print(oqi)
+#file=open("olma.txt","r")
+#oqi=file.read()
+#print(oqi)
+try:
+    #file=open("tex.txt","w")
+    #oqi=file.write("tushundim")
+    #print(oqi)
+    file=open("text.txt","r")
+    oqi=file.read()
+    print(oqi)
+except:
+    print("xatolik")
+
