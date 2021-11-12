0084b9c3d5aa6d14a3c7d6316063ea51fd2748f9
Mamajonova <mamajonovagulkhayokhon@gmail.com>
2021-11-12 15:53:09 +0500

    SLEEP

---
+++ sleep.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 17:15:28 2021
+
+@author: Premium
+"""
+
+import time
+#time=time.ctime()
+#print(time)        hozirgi vaqtni chiqarib beradi
+
+
+
+#print("1-ma'lumot")
+#time=time.sleep(10)  # sleep to'xtatib qo'yish
+#print("2-ma'lumot") # 1-ma'lumot chiqqandan 10 soniyadan keyin 2-ma'lumot chiqadi
+
+
+#def poyga():
+#    print("Tayyorgarlik ko'rilsin")
+#    time.sleep(2)
+#    print("1")
+#    time.sleep(1)
+#    print("2")
+#    time.sleep(1)
+#    print("3")
+#    time.sleep(1)
+#    print("Start")
+#poyga()
+
+
+
+def svetafor():
+    print("Svetafor")
+    print("QIZIL")
+    print("Barcha transport vozitalari to'xtasin")
+    time.sleep(30)
+    print("SARIQ")
+    print("Harakatlanishga tsyyorlaning")
+    time.sleep(5)
+    print("YASHIL")
+    print("Transportlar harakati boshlandi. Piyodalar to'xtang")
+    time.sleep(30)
+    print("SARIQ")
+    print("Transport vositalari to'xtashga tayyorlaning.")
+    time.sleep(5)
+    print("QIZIL")
+    print("To'xtang")
+svetafor()
